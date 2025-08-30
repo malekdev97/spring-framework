154c13a9a27f29d1d65d10543f90836cbc5deab1
Abdulmalik <abdulmalik-abdullah@hotmail.com>
2025-08-30 03:04:23 +0300

    call variable from application.properties

--- src/main/java/com/artcode/artcode/HomeController.java
+++ src/main/java/com/artcode/artcode/HomeController.java
@@ -1,5 +1,6 @@
 package com.artcode.artcode;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
+    @Value("${spring.application.name}")
+    private String appName;
+
     @GetMapping("/")
     public ResponseEntity<?> home() {
 
-        return ResponseEntity.ok("It's working fine!");
+        return ResponseEntity.ok(appName);
     }
 }
