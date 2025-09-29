a1dbbf8a17adc1f0e9c1aebafde2c073dcedc4a8
Abdulmalik <abdulmalik-abdullah@hotmail.com>
2025-09-30 00:37:07 +0300

    fresh start

--- src/main/java/com/artcode/artcode/ArtcodeApplication.java
+++ src/main/java/com/artcode/artcode/ArtcodeApplication.java
@@ -1,8 +1,5 @@
 package com.artcode.artcode;
 
-import com.artcode.artcode.services.EmailNotificationService;
-import com.artcode.artcode.services.NotificationService;
-import com.artcode.artcode.services.SMSNotificationService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -12,9 +9,6 @@ public class ArtcodeApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(ArtcodeApplication.class, args);
-
-		NotificationManager manager = context.getBean(NotificationManager.class);
-		manager.sendNotification("Hello Spring");
 	}
 
 }
--- src/main/java/com/artcode/artcode/services/NotificationService.java
+++
@@ -1,9 +0,0 @@
-package com.artcode.artcode.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface NotificationService {
-
-    public void send(String message);
-}
